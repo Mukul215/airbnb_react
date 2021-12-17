b5402e89f249ba4392c2b5e51129261f21297b22
Mukul Patel <Mukul215@gmail.com>
2021-12-17 11:44:45 -0500

    fixed .git in folder

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderImage from './HeaderImage'
+
+const Header = () => {
+  return (
+    <div>
+      <HeaderImage />
+    </div>
+  )
+}
+
+export default Header
