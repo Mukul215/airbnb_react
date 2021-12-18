887fd064c3888a43db11449fd883cce8ad7007b9
Mukul Patel <Mukul215@gmail.com>
2021-12-18 17:54:42 -0500

    added dynamic Card details to take props

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,28 +3,28 @@ import CardImage from "./CardImage"
 import cardData from "./data"
 
 const Card = () => {
-  console.log(cardData[0].coverImg)
-
-
   const heroImage = cardData.map((item, index) => {
     return (
         <CardImage img={item.coverImg} />
     )
   })
 
+  const heroDetail = cardData.map((item, index) => {
+    return (
+        <CardDetail 
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          country={item.location}
+          title={item.title}
+          price={item.price}
+        />  
+    )
+  })
+
   return (
     <div className="container max-w-screen-lg mx-auto pb-10">
-      {/* {heroImage}       */}
-      <CardImage 
-        img="Katie.png"
-      /> 
-      <CardDetail               
-        rating="5.0"
-        reviewCount={100}
-        country="United States"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      {heroImage}      
+      {heroDetail}
     </div>
   )
 }
--- src/components/card/CardImage.jsx
+++ src/components/card/CardImage.jsx
@@ -2,7 +2,7 @@ const CardImage = ({img}) => {
   return (
     <div className="inline">
       <div className="absolute bg-white border-2 rounded-sm font-Poppins font-thin my-2 mx-2 text-sm px-1">SOLD OUT</div>
-      <img src={require(`../../assets/${img}`)} alt="Hero" />
+      <img className="w-1/4 h-min rounded-2xl" src={require(`../../assets/${img}`)} alt="Hero" />
     </div>
   )
 }
